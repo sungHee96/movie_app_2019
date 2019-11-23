f3e138f3a76a2e67f5627a9baeb8fa62746dd8d6
sungHee96 <nsh722@naver.com>
2019-11-23 21:43:36 +0900

    첫번째 내용

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+function Food({fav}){
+return <h4>성희씨는 {fav}!! 아주 좋아한다!!</h4>
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(
+    <div> 
+      <h1>Hello!!!!! 나성희씨의 리액트 공부</h1>
+      <Food fav="고봉민 돈까스 김밥" />
+      <Food fav="BHC 치즈볼" />
+      <Food fav="광어지느러미" />
     </div>
-  );
+    );
 }
 
 export default App;
